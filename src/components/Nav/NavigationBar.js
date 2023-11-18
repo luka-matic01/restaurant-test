f45642f8f5e97775a6852eaea01b5ede4c94e600
m4t4a <lukamatic129@gmail.com>
2023-11-18 11:39:00 +0100

    Succesfully finished navbar

--- src/components/Nav/NavigationBar.js
+++ src/components/Nav/NavigationBar.js
@@ -1,131 +1,179 @@
 "use client";
 import Image from "next/image";
-import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
-import { useEffect, useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import React, { useState, useEffect } from "react";
+import {
+  AiOutlineMenu,
+  AiOutlineClose,
+  AiFillInstagram,
+  AiFillFacebook,
+} from "react-icons/ai";
 
-function NavBar() {
-  const [navbar, setNavbar] = useState(false);
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const [color, setColor] = useState("transparent");
+  const [textColor, setTextColor] = useState("white");
   const pathname = usePathname();
 
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
   useEffect(() => {
-    setNavbar(false);
+    const changeColor = () => {
+      if (window.scrollY >= 90) {
+        setColor("#ffffff");
+        setTextColor("#000000");
+      } else {
+        setColor("transparent");
+        setTextColor("#ffffff");
+      }
+    };
+    window.addEventListener("scroll", changeColor);
   }, []);
 
-  const backgroundColor =
-    pathname === "/" && navbar === false ? "transparent" : "[#36394c]";
   return (
-    <nav
-      className={`w-full relative bg-transparent ${
-        pathname === "/" && "absolute  top-0 left-0 right-0"
-      } z-50`}
+    <div
+      style={{ backgroundColor: `${color}` }}
+      className="fixed  left-0 top-0 w-full z-50 ease-in duration-300"
     >
-      <div className="justify-between items-center px-4 md:text-[15px] lg:text-[16px] mx-auto uppercase lg:max-w-7xl lg:items-center lg:flex lg:px-8">
-        <div>
-          <div className="flex items-center justify-between py-3 lg:py-5 lg:block">
-            {/* LOGO */}
-            <Link href="/">
-              <Image
-                src="/restaurant-logo.png"
-                alt="Hedonist logo"
-                width={400}
-                height={100}
-              />
-              {/* <Image
-                src="/restaurant-logo1.png"
-                alt="Hedonist logo"
-                className="lg:hidden block"
-                width={100}
-                height={100}
-              /> */}
-            </Link>
-            {/* HAMBURGER BUTTON FOR MOBILE */}
-            <div className="lg:hidden">
-              <button
-                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
-                onClick={() => setNavbar(!navbar)}
-              >
-                {navbar ? (
-                  <AiOutlineClose size={25} className="text-white" />
-                ) : (
-                  <AiOutlineMenu size={25} className="text-white" />
-                )}
-              </button>
-            </div>
-          </div>
-        </div>
-        <div>
-          <div
-            className={`flex-1  justify-self-center pb-3 mt-8 lg:block lg:pb-0 lg:mt-0 ${
-              navbar ? "p-12 lg:p-0 block" : "hidden"
-            }`}
+      <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white">
+        <Link href="/">
+          <Image
+            src="/restaurant-logo.png"
+            alt="Hedonist logo"
+            width={400}
+            height={100}
+          />
+        </Link>
+        <ul style={{ color: `${textColor}` }} className="hidden lg:flex">
+          <li
+            className={`${
+              pathname === "/" ? "text-[#f28274]" : `text-[${textColor}]`
+            } p-4 hover:decoration-[#f28274] hover:underline font-bold`}
           >
-            <ul
-              className="h-screen lg:h-auto space-y-10 lg:space-y-0 items-center justify-center lg:flex "
-              style={{ zIndex: 999 }}
+            <Link href="/">Početna</Link>
+          </li>
+          <li
+            className={`${
+              pathname === "/menu" ? "text-[#f28274]" : `text-[${textColor}]`
+            } p-4 hover:decoration-[#f28274] hover:underline font-bold`}
+          >
+            <Link href="/menu">Meni</Link>
+          </li>
+          <li
+            className={`${
+              pathname === "/gallery" ? "text-[#f28274]" : `text-[${textColor}]`
+            } p-4 hover:decoration-[#f28274] hover:underline font-bold`}
+          >
+            <Link href="/gallery">Galerija</Link>
+          </li>
+          <li
+            className={`${
+              pathname === "/about" ? "text-[#f28274]" : `text-[${textColor}]`
+            } p-4 hover:decoration-[#f28274] hover:underline font-bold`}
+          >
+            <Link href="/about">O nama</Link>
+          </li>
+          <li
+            className={`${
+              pathname === "/contact" ? "text-[#f28274]" : `text-[${textColor}]`
+            } p-4 hover:decoration-[#f28274] hover:underline font-bold`}
+          >
+            <Link href="/contact">Kontakt</Link>
+          </li>
+        </ul>
+
+        {/* Mobile Button */}
+        <div onClick={handleNav} className="block lg:hidden z-10">
+          {nav ? (
+            <AiOutlineClose
+              size={30}
+              style={{ color: `${textColor}` }}
+              className="cursor-pointer"
+            />
+          ) : (
+            <AiOutlineMenu
+              size={30}
+              style={{ color: `${textColor}` }}
+              className="cursor-pointer"
+            />
+          )}
+        </div>
+        {/* Mobile Menu */}
+        <div
+          className={
+            nav
+              ? "lg:hidden absolute flex-col top-0 left-0 right-0 bottom-0 flex justify-between pb-6 pt-3 md:pt-4 items-center w-full h-screen bg-[#36394c] text-center ease-in duration-300"
+              : "lg:hidden absolute flex-col top-0 left-[-100%] right-0 bottom-0 flex justify-between pb-6 pt-4 items-center w-full h-screen bg-[#36394c] text-center ease-in duration-300"
+          }
+        >
+          <Image
+            src="/restaurant-logo.png"
+            alt="Hedonist logo"
+            width={400}
+            height={100}
+          />
+          <ul>
+            <li
+              onClick={handleNav}
+              className={`${
+                pathname === "/" && "text-[#f28274]"
+              } p-4 text-4xl hover:decoration-[#f28274] hover:underline`}
             >
-              <li className="  text-white py-2 lg:px-6 text-center border-b-2 lg:border-b-0     lg:hover:bg-transparent">
-                <Link
-                  href="/"
-                  onClick={() => setNavbar(!navbar)}
-                  className={`hover:decoration-[#f28274] hover:underline ${
-                    pathname === "/" ? "text-[#f28274]" : "text-white"
-                  }`}
-                >
-                  Početna
-                </Link>
-              </li>
-              <li className="  text-white py-2 px-6 text-center  border-b-2 lg:border-b-0     lg:hover:bg-transparent">
-                <Link
-                  href="/menu"
-                  onClick={() => setNavbar(!navbar)}
-                  className={`hover:decoration-[#f28274] hover:underline ${
-                    pathname === "/menu" ? "text-[#f28274]" : "text-white"
-                  }`}
-                >
-                  Meni
-                </Link>
-              </li>
-              <li className="  text-white py-2 px-6 text-center  border-b-2 lg:border-b-0     lg:hover:bg-transparent">
-                <Link
-                  href="/gallery"
-                  onClick={() => setNavbar(!navbar)}
-                  className={`hover:decoration-[#f28274] hover:underline ${
-                    pathname === "/gallery" ? "text-[#f28274]" : "text-white"
-                  }`}
-                >
-                  Galerija
-                </Link>
-              </li>
-              <li className="  text-white py-2 px-6 text-center  border-b-2 lg:border-b-0     lg:hover:bg-transparent">
-                <Link
-                  href="/about"
-                  onClick={() => setNavbar(!navbar)}
-                  className={`hover:decoration-[#f28274] hover:underline ${
-                    pathname === "/about" ? "text-[#f28274]" : "text-white"
-                  }`}
-                >
-                  O nama
-                </Link>
-              </li>
-              <li className="  text-white py-2 px-6 text-center  border-b-2 lg:border-b-0     lg:hover:bg-transparent">
-                <Link
-                  href="/contact"
-                  onClick={() => setNavbar(!navbar)}
-                  className={`hover:decoration-[#f28274] hover:underline ${
-                    pathname === "/contact" ? "text-[#f28274]" : "text-white"
-                  }`}
-                >
-                  Kontakt
-                </Link>
-              </li>
-            </ul>
+              <Link href="/">Početna</Link>
+            </li>
+            <li
+              onClick={handleNav}
+              className={`${
+                pathname === "/menu" && "text-[#f28274]"
+              } p-4 text-4xl hover:decoration-[#f28274] hover:underline`}
+            >
+              <Link href="/menu">Meni</Link>
+            </li>
+            <li
+              onClick={handleNav}
+              className={`${
+                pathname === "/gallery" && "text-[#f28274]"
+              } p-4 text-4xl hover:decoration-[#f28274] hover:underline`}
+            >
+              <Link href="/gallery">Galerija</Link>
+            </li>
+            <li
+              onClick={handleNav}
+              className={`${
+                pathname === "/about" && "text-[#f28274]"
+              } p-4 text-4xl hover:decoration-[#f28274] hover:underline`}
+            >
+              <Link href="/about">O nama</Link>
+            </li>{" "}
+            <li
+              onClick={handleNav}
+              className={`${
+                pathname === "/contact" && "text-[#f28274]"
+              } p-4 text-4xl hover:decoration-[#f28274] hover:underline`}
+            >
+              <Link href="/contact">Kontakt</Link>
+            </li>
+          </ul>
+          <div className="flex flex-col items-center justify-center w-full">
+            <Image src="/line.png" width={120} height={2} alt="linija" />
+            <h1 className="text-white text-lg">since 2020</h1>
+          </div>
+
+          <div className="flex items-center justify-center gap-4">
+            <h1 className=" bg-[#f28274] p-3 rounded-full">
+              <AiFillInstagram size={25} className="text-white" />
+            </h1>
+            <h1 className=" bg-[#f28274] p-3 rounded-full">
+              <AiFillFacebook size={25} className="text-white" />
+            </h1>
           </div>
         </div>
       </div>
-    </nav>
+    </div>
   );
-}
+};
 
-export default NavBar;
+export default Navbar;
